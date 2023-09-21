8f2493e8a400eec4ffc8bfa08717fc9c10ff8f6c
StanislavSlobodenyuk <116057509+StanislavSlobodenyuk@users.noreply.github.com>
2023-09-21 13:18:06 +0300

    Добавьте файлы проекта.

---
+++ SportsStore/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using SportsStore.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<StoreDbContext>(opts => {
+    opts.UseSqlServer(
+    builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+});
+
+// EF repo-intefaces
+builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+
+builder.Services.AddRazorPages();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+
+SeedData.EnsurePopulated(app);
+
+app.UseStaticFiles();
+app.UseSession();
+
+app.MapControllerRoute("pagination", "Produt/Page{productPage}",
+    new { Controller = "Home,", action = "Index" });
+
+app.MapControllerRoute("catpage",
+    "{category}/Page{productPage:int}",
+    new { Controller = "Home", action = "Index" });
+
+app.MapControllerRoute("page", "Page{productPage:int}",
+    new { Controller = "Home", action = "Index", productPage = 1 });
+
+app.MapControllerRoute("category", "{category}",
+    new { Controller = "Home", action = "Index", productPage = 1 });
+
+app.MapControllerRoute("pagination",
+    "Products/Page{productPage}",
+    new { Controller = "Home", action = "Index", productPage = 1 });
+
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
+
+app.Run();
